80201b949d2f3641362aaee1ed7f58dd40cba9d6
zKauaFerreira <kaua@agencyos.com>
2026-05-08 21:10:06 -0300

    feat: require admin startup for v1.0.6

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    tauri_build::build()
+    let windows = tauri_build::WindowsAttributes::new().app_manifest(include_str!("admin-manifest.xml"));
+    let attributes = tauri_build::Attributes::new().windows_attributes(windows);
+    tauri_build::try_build(attributes).expect("failed to run tauri build script");
 }
